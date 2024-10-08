b71d1b76ae14b1c71da049a8264662198dc82a3b
ismo177 <iissmmoo177@gmail.com>
2024-10-09 00:01:26 +0200

    MainFrame cleaning...

--- src/main/java/gui/MainFrame.java
+++ src/main/java/gui/MainFrame.java
@@ -9,7 +9,6 @@ import javax.swing.*;
 import javax.swing.border.LineBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -17,7 +16,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public class MainFrame extends JFrame implements ActionListener {
+public class MainFrame extends JFrame  {
 
     Toolkit tk = Toolkit.getDefaultToolkit();
     Dimension screenSize = tk.getScreenSize();
@@ -26,84 +25,11 @@ public class MainFrame extends JFrame implements ActionListener {
     JButton animalsButton, moviesButton, sportsButton, scienceButton, musicButton, foodButton;
     JLabel headerLabel, authorLabel;
     JButton startQuizButton, aboutButton, instructionsButton, pastScoresButton, exitButtom;
-
     int tempCategorie;
     JLabel playerLabel;
     User tempUser;
     MainFrame(User tempUser){
         this.tempUser = tempUser;
-        /*
-        headerLabel = new JLabel("Quiz  Game", JLabel.CENTER);
-        headerLabel.setLayout(null);
-        headerLabel.setLocation(0,0);
-        headerLabel.setSize(screenSize.width-15,100);
-
-        startQuizButton=new JButton("Start Quiz");
-        aboutButton =new JButton("About");
-        instructionsButton =new JButton("Instructions");
-        pastScoresButton =new JButton("Past Scores");
-        exitButtom =new JButton("Exit");
-
-        authorLabel=new JLabel("<html><b>Ismet Omerović</b><br>It Akademija<br>2023/24</html>");
-        authorLabel.setSize(250,100);
-        authorLabel.setLocation(screenSize.width-150,20);
-
-        playerLabel =new JLabel("Player: "+ tempUser.getUsername());
-        playerLabel.setSize(280,50);
-        playerLabel.setLocation(40,95);
-
-        ImageIcon icon1=new ImageIcon(ClassLoader.getSystemResource("images/animals.png"));
-        Image image1=icon1.getImage();
-        Image newImage1=image1.getScaledInstance(310,280,Image.SCALE_DEFAULT);
-        ImageIcon icon11=new ImageIcon(newImage1);
-        animalsButton =new JButton(icon11);
-
-
-
-        ImageIcon icon2=new ImageIcon(ClassLoader.getSystemResource("images/films.jpg"));
-        Image image2=icon2.getImage();
-        Image newImage2=image2.getScaledInstance(310,280,Image.SCALE_DEFAULT);
-        ImageIcon icon22=new ImageIcon(newImage2);
-        moviesButton =new JButton(icon22);
-        moviesButton.setFont(new Font("Serif", Font.BOLD, 24));
-
-
-        ImageIcon icon3=new ImageIcon(ClassLoader.getSystemResource("images/sports.jpg"));
-        Image image3=icon3.getImage();
-        Image newImage3=image3.getScaledInstance(310,280,Image.SCALE_DEFAULT);
-        ImageIcon icon33=new ImageIcon(newImage3);
-        sportsButton =new JButton(icon33);
-        sportsButton.setFont(new Font("Serif", Font.BOLD, 24));
-
-        ImageIcon icon4=new ImageIcon(ClassLoader.getSystemResource("images/science.jpg"));
-        Image image4=icon4.getImage();
-        Image newImage4=image4.getScaledInstance(310,280,Image.SCALE_DEFAULT);
-        ImageIcon icon44=new ImageIcon(newImage4);
-        scienceButton =new JButton(icon44);
-        scienceButton.setFont(new Font("Serif", Font.BOLD, 24));
-
-        ImageIcon icon5=new ImageIcon(ClassLoader.getSystemResource("images/music.png"));
-        Image image5=icon5.getImage();
-        Image newImage5=image5.getScaledInstance(310,280,Image.SCALE_DEFAULT);
-        ImageIcon icon55=new ImageIcon(newImage5);
-        musicButton =new JButton(icon55);
-        musicButton.setFont(new Font("Serif", Font.BOLD, 24));
-
-        ImageIcon icon6=new ImageIcon(ClassLoader.getSystemResource("images/food.png"));
-        Image image6=icon6.getImage();
-        Image newImage6=image6.getScaledInstance(310,280,Image.SCALE_DEFAULT);
-        ImageIcon icon66=new ImageIcon(newImage6);
-        foodButton =new JButton(icon66);
-        foodButton.setFont(new Font("Serif", Font.BOLD, 24));
-
-         */
-
-
-
-
-
-
-
 
         createComponents();
         createFrame();
@@ -141,42 +67,75 @@ public class MainFrame extends JFrame implements ActionListener {
         mainPanel.setBackground(new Color(208, 202, 253));
         mainPanel.setLocation(0,0);
         mainPanel.setSize(screenSize.width,screenSize.height);
-
     }
 
-    public void createComponents(){
-        JButton[] categoriePanelButtons= {animalsButton, moviesButton, sportsButton, scienceButton, musicButton, foodButton};
-        String[] categorieNames= {"animals", "movies", "sports", "science", "music", "food"};
+    public void createComponents() {
         headerLabel = new JLabel("Quiz  Game", JLabel.CENTER);
         headerLabel.setLayout(null);
-        headerLabel.setLocation(0,0);
-        headerLabel.setSize(screenSize.width-15,100);
+        headerLabel.setLocation(0, 0);
+        headerLabel.setSize(screenSize.width - 15, 100);
 
-        startQuizButton=new JButton("Start Quiz");
-        aboutButton =new JButton("About");
-        instructionsButton =new JButton("Instructions");
-        pastScoresButton =new JButton("Past Scores");
-        exitButtom =new JButton("Exit");
+        startQuizButton = new JButton("Start Quiz");
+        aboutButton = new JButton("About");
+        instructionsButton = new JButton("Instructions");
+        pastScoresButton = new JButton("Past Scores");
+        exitButtom = new JButton("Exit");
 
-        authorLabel=new JLabel("<html><b>Ismet Omerović</b><br>It Akademija<br>2023/24</html>");
-        authorLabel.setSize(250,100);
-        authorLabel.setLocation(screenSize.width-150,20);
+        authorLabel = new JLabel("<html><b>Ismet Omerović</b><br>It Akademija<br>2023/24</html>");
+        authorLabel.setSize(250, 100);
+        authorLabel.setLocation(screenSize.width - 150, 20);
 
-        playerLabel =new JLabel("Player: "+ tempUser.getUsername());
-        playerLabel.setSize(280,50);
-        playerLabel.setLocation(40,95);
+        playerLabel = new JLabel("Player: " + tempUser.getUsername());
+        playerLabel.setSize(280, 50);
+        playerLabel.setLocation(40, 95);
 
-        for(int i=0; i<categoriePanelButtons.length; i++) {
-            ImageIcon imageIcon = new ImageIcon(ClassLoader.getSystemResource("images/"+categorieNames[i]+".png"));
-            Image image = imageIcon.getImage();
-            Image newImage = image.getScaledInstance(310, 280, Image.SCALE_DEFAULT);
-            ImageIcon newImageIcon = new ImageIcon(newImage);
-            categoriePanelButtons[i] = new JButton(newImageIcon);
-        }
+
+        ImageIcon icon1=new ImageIcon(ClassLoader.getSystemResource("images/animals.png"));
+        Image image1=icon1.getImage();
+        Image newImage1=image1.getScaledInstance(310,280,Image.SCALE_DEFAULT);
+        ImageIcon icon11=new ImageIcon(newImage1);
+        animalsButton =new JButton(icon11);
+
+        ImageIcon icon2=new ImageIcon(ClassLoader.getSystemResource("images/films.png"));
+        Image image2=icon2.getImage();
+        Image newImage2=image2.getScaledInstance(310,280,Image.SCALE_DEFAULT);
+        ImageIcon icon22=new ImageIcon(newImage2);
+        moviesButton =new JButton(icon22);
+        moviesButton.setFont(new Font("Serif", Font.BOLD, 24));
+
+
+        ImageIcon icon3=new ImageIcon(ClassLoader.getSystemResource("images/sports.png"));
+        Image image3=icon3.getImage();
+        Image newImage3=image3.getScaledInstance(310,280,Image.SCALE_DEFAULT);
+        ImageIcon icon33=new ImageIcon(newImage3);
+        sportsButton =new JButton(icon33);
+        sportsButton.setFont(new Font("Serif", Font.BOLD, 24));
+
+        ImageIcon icon4=new ImageIcon(ClassLoader.getSystemResource("images/science.png"));
+        Image image4=icon4.getImage();
+        Image newImage4=image4.getScaledInstance(310,280,Image.SCALE_DEFAULT);
+        ImageIcon icon44=new ImageIcon(newImage4);
+        scienceButton =new JButton(icon44);
+        scienceButton.setFont(new Font("Serif", Font.BOLD, 24));
+
+        ImageIcon icon5=new ImageIcon(ClassLoader.getSystemResource("images/music.png"));
+        Image image5=icon5.getImage();
+        Image newImage5=image5.getScaledInstance(310,280,Image.SCALE_DEFAULT);
+        ImageIcon icon55=new ImageIcon(newImage5);
+        musicButton =new JButton(icon55);
+        musicButton.setFont(new Font("Serif", Font.BOLD, 24));
+
+        ImageIcon icon6=new ImageIcon(ClassLoader.getSystemResource("images/food.png"));
+        Image image6=icon6.getImage();
+        Image newImage6=image6.getScaledInstance(310,280,Image.SCALE_DEFAULT);
+        ImageIcon icon66=new ImageIcon(newImage6);
+        foodButton =new JButton(icon66);
+        foodButton.setFont(new Font("Serif", Font.BOLD, 24));
 
         addFont();
-        createBackPanels();
         addBackColorForMenuPanelButtons();
+        createBackPanels();
+
     }
 
     public void addFont(){
@@ -208,8 +167,6 @@ public class MainFrame extends JFrame implements ActionListener {
     }
 
 
-
-
     public void addComponents(){
         JButton[] menuBarButtons= { startQuizButton, aboutButton, instructionsButton, pastScoresButton, exitButtom };
         JButton[] categoriesPanelButtons={ animalsButton, moviesButton, sportsButton, scienceButton, musicButton, foodButton };
@@ -231,39 +188,37 @@ public class MainFrame extends JFrame implements ActionListener {
         JButton[] categoriesPanelButtons={animalsButton, moviesButton, sportsButton, scienceButton, musicButton, foodButton};
         //JButton[]  menuBarPanelButtons= { aboutButton, instructionsButton, pastScoresButton, exitButtom };
         for(JButton button: categoriesPanelButtons){
-            button.addActionListener(this);
+            button.addActionListener(this::chooseCategory);
         }
         startQuizButton.addActionListener(this::onStartQuizClick);
         //aboutButton.addActionListener(this::);
         //instructionsButton.addActionListener(this::);
-        pastScoresButton.addActionListener(this::onClickScoresButton);
-        startQuizButton.addActionListener(this::onClickExitButton);
+        pastScoresButton.addActionListener(this::onClickGetScoresButton);
+        exitButtom.addActionListener(this::onClickExitButton);
  }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == animalsButton) {
-            tempCategorie = 1;
-            System.out.println("Category= " + tempCategorie);
-        } else if (e.getSource() == moviesButton) {
-            tempCategorie = 2;
-            System.out.println("Category= " + tempCategorie);
-        } else if (e.getSource() == sportsButton) {
-            tempCategorie = 3;
-            System.out.println("Category= " + tempCategorie);
-        } else if (e.getSource() == scienceButton) {
-            tempCategorie = 4;
-            System.out.println("Category= " + tempCategorie);
-        } else if (e.getSource() == musicButton) {
-            tempCategorie = 5;
-            System.out.println("Category= " + tempCategorie);
-        } else if (e.getSource() == foodButton) {
-            tempCategorie = 6;
-            System.out.println("Category= " + tempCategorie);
-        }
 
-        }
-
+public void chooseCategory(ActionEvent e){
+    if (e.getSource() == animalsButton) {
+        tempCategorie = 1;
+        System.out.println("Category= " + tempCategorie);
+    } else if (e.getSource() == moviesButton) {
+        tempCategorie = 2;
+        System.out.println("Category= " + tempCategorie);
+    } else if (e.getSource() == sportsButton) {
+        tempCategorie = 3;
+        System.out.println("Category= " + tempCategorie);
+    } else if (e.getSource() == scienceButton) {
+        tempCategorie = 4;
+        System.out.println("Category= " + tempCategorie);
+    } else if (e.getSource() == musicButton) {
+        tempCategorie = 5;
+        System.out.println("Category= " + tempCategorie);
+    } else if (e.getSource() == foodButton) {
+        tempCategorie = 6;
+        System.out.println("Category= " + tempCategorie);
+    }
+}
 
 
     public void addBackColorForMenuPanelButtons(){
@@ -301,7 +256,6 @@ public class MainFrame extends JFrame implements ActionListener {
             List<UserScore> list=new ArrayList<>();
             UserService userService=new UserService();
 
-
             for(String string : playerScoresList){
                 String[] parts = string.split(";");
                 String name = parts[0];
@@ -333,8 +287,7 @@ public class MainFrame extends JFrame implements ActionListener {
         }
     }
 
-    public void onClickScoresButton(ActionEvent actionEvent) {
-        //playerName= tempUser.getUsername();
+    public void onClickGetScoresButton(ActionEvent actionEvent) {
         UserScoreService userScoreService=new UserScoreService();
         List<UserScore> scoresList=userScoreService.findAll();
         new ScoresFrame(scoresList);
